170e665a814ceeb80155348d70f19ee4ebf6dbff
Yuanhong-Lan <844190557@qq.com>
2024-12-12 17:27:07 +0800

    feat: add time string check

--- ordinary/time_operation.py
+++ ordinary/time_operation.py
@@ -3,9 +3,18 @@
 # @Author: Yuanhong Lan
 # ----------------------
 import time
+from dateutil.parser import parse
 
 
 class TimeOperation:
     @classmethod
     def get_local_time_str(cls):
         return time.strftime('%Y-%m-%d-%H:%M:%S', time.localtime())
+
+    @classmethod
+    def is_a_time_string(cls, s):
+        try:
+            parse(s, fuzzy=False)
+            return True
+        except ValueError:
+            return False
