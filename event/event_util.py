e2bbd18de59f166e1d9ace17a95b389d464c1319
Yuanhong-Lan <844190557@qq.com>
2024-12-24 15:00:12 +0800

    feat: update event util

--- event/event_util.py
+++ event/event_util.py
@@ -6,16 +6,20 @@
 class EventUtil:
     @staticmethod
     def is_editable(s: str):
-        return (".EditText" in s) or ("AutoCompleteTextView" in s)
+        return ("EditText" in s) or ("AutoCompleteTextView" in s)
 
     @staticmethod
     def is_seekbar(s: str):
-        return ".SeekBar" in s
+        return "SeekBar" in s
 
     @staticmethod
     def is_switch(s: str):
-        return ".Switch" in s
+        return "Switch" in s
 
     @staticmethod
     def is_text_presentation(s: str):
         return ("TextView" in s) or ("android.view.View" == s)
+
+    @staticmethod
+    def is_list(s: str):
+        return ("ListView" in s) or ("RecyclerView" in s)
